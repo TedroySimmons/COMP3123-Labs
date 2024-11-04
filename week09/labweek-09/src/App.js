eac8ef45852f56aecc8e602adbd6f3b4ea7e5b99
TedroySimmons <tedroy.simmons@georgebrown.ca>
2024-11-03 21:53:26 -0500

    lab

---
+++ week09/labweek-09/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Fullstack Development - 1</h1>
+        <h2>React JS Programming Week09 Lab exercise</h2>
+        <h3>101066725</h3>
+        <h4>Tedroy Simmons</h4>
+        <h5>George Brown College, Toronto</h5>
+      </header>
+    </div>
+  );
+}
+
+export default App;
